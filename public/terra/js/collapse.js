d98e98e07d4aca7cac39d468142cf063e9fcde0c
Jordan Gold <jordandgold@gmail.com>
2018-07-18 08:32:16 -0600

    search select updates

--- public/terra/js/collapse.js
+++ public/terra/js/collapse.js
@@ -1,13 +1,12 @@
 jQuery(document).ready(function($){
 
     $('.accordion__heading').click(function () {
-    	/* Collapse the sibling accordions if this has a group */
+    	// Collapse the sibling accordions if this has a group
 		var maybeGroup = $(this).parent().parent();
     	if(maybeGroup && maybeGroup.hasClass('accordion-group')) {
     		$(this).parent().siblings('.accordion').removeClass('expanded');
     	}
-
-    	/* Adjust the one you clicked */
+    	// Adjust the one you clicked
     	$(this).parent().toggleClass('expanded');
     });
           
--- public/terra/js/multiselect.js
+++ public/terra/js/multiselect.js
@@ -1,21 +1,6 @@
 jQuery(document).ready(function($){
 
-	// function filterFunction() {
-	//   var input, filter, ul, li, a, i;
-	//   input = $('input#search-filter');
-	//   filter = input.value.toUpperCase();
-	//   div = $(".dropdown-search__results");
-	//   a = div.getElementsByTagName("a");
-	//   for (i = 0; i < a.length; i++) {
-	//     if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-	//       a[i].style.display = "";
-	//     } else {
-	//       a[i].style.display = "none";
-	//     }
-	//   }
-	// }
-
-	$('select').each(function(){
+	$('select:not([multiple])').each(function(){
 	    var $this = $(this), numberOfOptions = $(this).children('option').length;
 	  
 	    $this.addClass('select-hidden'); 
@@ -45,8 +30,7 @@ jQuery(document).ready(function($){
 		        'placeholder': 'search'
 	    	}).appendTo($searchFilter);
 
-	    	// $list.append(searchFilterMarkup);
-	    	// $list.append('<div rel="hide" class="no-results">No results found</div>');
+	    	$list.append('<div rel="hide" class="no-results">No results found</div>');
 
 	    	$searchFilterInput.on('keyup', function(){
 	    		var options = $('.select-options-wrap').children('li:not([rel="hide"])'),
@@ -94,19 +78,148 @@ jQuery(document).ready(function($){
 	        $(this).toggleClass('active').next('ul.select-options').toggleClass('show');
 	    });
 	  
+		//
 	    $listItems.click(function(e) {
 	        e.stopPropagation();
+	        // replace text with selected option
 	        $styledSelect.text($(this).text()).removeClass('active');
 	        $this.val($(this).attr('rel'));
 	        $list.removeClass('show');
-	        //console.log($this.val());
+	        console.log($this.val());
 	    });
-	  
+	  	
+		// hide dropdown when you click out
 	    $(document).click(function() {
 	        $styledSelect.removeClass('active');
 	        $list.removeClass('show');
 	    });
 
+		// don't close the select if a user focuses on the search
+	    $($searchFilterInput).click(function(event){
+		    event.stopPropagation();
+		});
+
+	});
+
+	$('select[multiple]').each(function(){
+	    var $this = $(this), numberOfOptions = $(this).children('option').length;
+	  
+	    $this.addClass('select-hidden'); 
+	    $this.wrap('<div class="select"></div>');
+	    $this.after('<div class="select-styled"></div>');
+
+	    var $styledSelect = $this.next('div.select-styled');
+	    $styledSelect.html('<span class="label">' + $this.children('option').eq(0).text() + '</div>');
+	  
+	    var $list = $('<ul />', {
+	        'class': 'select-options'
+    	}).insertAfter($styledSelect);
+
+	    // add search functionality
+    	if ($this.hasClass('search')) {
+
+    		// var searchFilterMarkup = '<div class="search-filter"><input type="text" id="searchFilter" placeholder="search" /></div>',
+    		// 	searchFilter = 'div.search-filter input';
+
+			var $searchFilter = $('<div />', {
+		        'class': 'search-filter'
+	    	}).appendTo($list);
+
+			var $searchFilterInput = $('<input />', {
+		        'id': 'searchFilter',
+		        'type': 'text',
+		        'placeholder': 'search'
+	    	}).appendTo($searchFilter);
+
+			// render no results
+	    	$list.append('<div rel="hide" class="no-results">No results found</div>');
+
+	    	// keyup search functionality
+	    	$searchFilterInput.on('keyup', function(){
+	    		var options = $('.select-options-wrap').children('li:not([rel="hide"])'),
+	    			filter = $(this).val().toUpperCase();
+				for (i = 0; i < options.length; i++) {
+
+					// count results
+					var resultsCount = $('.select-options-wrap').children('li:not([rel="hide"]):visible').length;
+
+					// check if any matching results
+					if (options[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+						options[i].style.display = "";
+					} else {
+						options[i].style.display = "none";
+					}
+
+					// display no results
+					if (resultsCount == 0) {
+						$('.no-results').show();
+					} else {
+						$('.no-results').hide();
+					}
+				}
+			});
+	    }
+
+	    var $optionsWrap = $('<div />', {
+	        'class': 'select-options-wrap'
+		}).appendTo($list);
+	  
+	    for (var i = 0; i < numberOfOptions; i++) {
+	        $('<li />', {
+	            text: $this.children('option').eq(i).text(),
+	            rel: $this.children('option').eq(i).val()
+	        }).appendTo($optionsWrap);
+	    }
+	  
+	    var $listItems = $list.children('.select-options-wrap').children('li');
+	  
+	    $styledSelect.click(function(event) {
+	        event.stopPropagation();
+	        $('div.select-styled.active').not(this).each(function(){
+	            $(this).removeClass('active').next('ul.select-options').toggleClass('show');
+	        });
+	        $(this).toggleClass('active').next('ul.select-options').toggleClass('show');
+	    });
+
+	    $listItems.click(function(event) {
+
+	        event.stopPropagation();
+	        
+	        // replace text with selected option
+	        $(this).hide();
+	        $styledSelect.find('span.label').hide();
+
+	        // close dropdown
+	        // $styledSelect.removeClass('active');
+	        $styledSelect.append('<span class="badge badge--info option" data-rel="' + $(this).attr('rel') + '">' + $(this).text() + '<button type="button" class="badge__close" aria-label="Remove Option"><span aria-hidden="true">Remove Option</span></button></span>');
+	        $this.val($(this).attr('rel'));
+	        // $list.removeClass('show');
+	        console.log($this.val());
+	    });
+
+	    // prevent dropdown from closing
+	    $styledSelect.on('click', '.option', function(event){
+	    	event.stopPropagation();
+	    });
+
+	    // close options
+	    $styledSelect.on('click', '.option button', function(event){
+	    	event.stopPropagation();
+	    	var rel = $(this).parent().attr('data-rel');
+	    	$listItems.next('li[rel="' + rel + '"]').show();
+			$(this).parent().remove();
+			if (!$styledSelect.find('.option').length) {
+	    		$styledSelect.find('.label').show();
+	    	}
+		});
+	  	
+		// hide dropdown when you click out
+	    $(document).click(function() {
+	        $styledSelect.removeClass('active');
+	        $list.removeClass('show');
+	    });
+
+		// don't close the select if a user focuses on the search
 	    $($searchFilterInput).click(function(event){
 		    event.stopPropagation();
 		});
