e374facecd45edba663e5262c450693346ebeaf3
Jordan Gold <jordandgold@gmail.com>
2018-07-20 12:40:46 -0600

    fix accordion animation

--- public/terra/js/collapse.js
+++ public/terra/js/collapse.js
@@ -1,15 +1,17 @@
 jQuery(document).ready(function($){
 
+    $('.accordion.expanded').children('.accordion__content').show();
+
     $('.accordion__heading').click(function () {
     	// Collapse the sibling accordions if this has a group
 		var maybeGroup = $(this).parent().parent();
     	if(maybeGroup && maybeGroup.hasClass('accordion-group')) {
     		$(this).parent().siblings('.accordion').removeClass('expanded');
-    		// $(this).parent().siblings('.accordion').children('.accordion__content').slideUp();
+    		$(this).parent().siblings('.accordion').find('.accordion__content').slideUp(200);
     	}
     	// Adjust the one you clicked
     	$(this).parent().toggleClass('expanded');
-    	// $(this).next('.accordion__content').slideDown();
+    	$(this).next('.accordion__content').slideToggle(200);
     });
 
 });
