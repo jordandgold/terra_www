567736753fb0c512634d84074c7e3b01c7d72da0
Jordan Gold <jordandgold@gmail.com>
2018-08-10 08:23:51 -0600

    clean up code

--- public/terra/js/dropdown.js
+++ public/terra/js/dropdown.js
@@ -5,26 +5,26 @@
 
 (function($, window, document){
   
-  'use strict';
+  	'use strict';
   
-  var $html = $('html');
+  	var $html = $('html');
   
-  $html.on('click', '.ter-dropdown', function(e) {
-    e.preventDefault();
-    if($(this).hasClass('is-open')) {
-      $(this).removeClass('is-open');
-      $('.ter-dropdown').removeClass('is-open');
-    } else {
-      $('.ter-dropdown').removeClass('is-open');
-      $(this).toggleClass('is-open');
-    }
-  });
+  	$html.on('click', '.ter-dropdown', function(e) {
+		e.preventDefault();
+		if($(this).hasClass('is-open')) {
+		  	$(this).removeClass('is-open');
+		  	$('.ter-dropdown').removeClass('is-open');
+	  	} else {
+		  	$('.ter-dropdown').removeClass('is-open');
+		  	$(this).toggleClass('is-open');
+	  	}
+	});
   
-  $html.on('click', function(e) {
-    var $target = $(e.target);
-    if (!$target.parents().hasClass('ter-dropdown')) {
-      $('.ter-dropdown').removeClass('is-open');
-    }
-  });
+	$html.on('click', function(e) {
+		var $target = $(e.target);
+		if (!$target.parents().hasClass('ter-dropdown')) {
+			$('.ter-dropdown').removeClass('is-open');
+		}
+	});
   
 })(jQuery, window, document);
