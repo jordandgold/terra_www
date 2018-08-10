587ba9a4e50558d479516168fda126cba45b2bcd
Jordan Gold <jordandgold@gmail.com>
2018-08-10 08:23:44 -0600

    refactor modal

--- public/terra/js/modal.js
+++ public/terra/js/modal.js
@@ -1,20 +1,27 @@
-jQuery(document).ready(function($){
+/**
+ * Terra Design System
+ * modal.js
+ */
+
+(function($, window, document){
+  
+	'use strict';
 
 	$('[data-toggle=modal]').click(function() {
 		var modalName = $(this).attr('data-target');
-		$(modalName).toggleClass('open');
-		$('body').toggleClass('modal-open');
+		$(modalName).toggleClass('is-open');
+		$('body').toggleClass('has-modal-open');
 		return false;
 	});
 	$('[data-close=modal]').click(function() {
-		$(this).parent().parent().parent().toggleClass('open');
-		$('body').toggleClass('modal-open');
+		$(this).parent().parent().parent().toggleClass('is-open');
+		$('body').toggleClass('has-modal-open');
 	});
 	$('.modal--full-page').click(function() {
-		$(this).toggleClass('open');
-		$('body').toggleClass('modal-open');
+		$(this).toggleClass('is-open');
+		$('body').toggleClass('has-modal-open');
 	}).children().click(function(e) {
 	  return false;
 	});
 
-}); 
+})(jQuery, window, document);
