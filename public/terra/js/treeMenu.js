678796dc8c4cc2faca1cc6c7eabe5a7105e80562
Jordan Gold <jordandgold@gmail.com>
2018-08-10 12:35:48 -0600

    fix initial expanded jumpy bug

--- public/terra/js/treeMenu.js
+++ public/terra/js/treeMenu.js
@@ -7,9 +7,15 @@
   
 	'use strict';
 
+	$('.tree-menu__list li.is-expanded').find('ul').show();
+
     $('.tree-menu__list li.is-expandable > a').click(function () {
-    	$(this).parent().siblings('.is-expanded').find('ul').slideToggle(200).parent().toggleClass('is-expanded');
-    	$(this).next('ul').slideToggle(200).parent().toggleClass('is-expanded');
+    	if ($(this).parent().hasClass('is-expanded')) {
+    		$(this).next('ul').slideToggle(200).parent().toggleClass('is-expanded');
+    	} else {
+    		$(this).parent().siblings('.is-expanded').find('ul').slideToggle(200).parent().toggleClass('is-expanded');
+    		$(this).next('ul').slideToggle(200).parent().toggleClass('is-expanded');
+    	}
     });     
 
 })(jQuery, window, document);
