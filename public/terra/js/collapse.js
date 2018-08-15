79ded44f50cc548434692adaa506dbf6cb0304f8
Jordan Gold <jordandgold@gmail.com>
2018-08-15 14:42:36 -0600

    update collapse to work outside accordions

--- public/terra/js/collapse.js
+++ public/terra/js/collapse.js
@@ -7,18 +7,31 @@
 
     'use strict';
     
+    // open all accordions
     $('.ter-accordion.is-expanded').children('.ter-accordion__content').show();
 
     $('[data-toggle="collapse"]').click(function () {
-    	// Collapse the sibling accordions if this has a group
-		var maybeGroup = $(this).parent().parent();
-    	if(maybeGroup && maybeGroup.hasClass('ter-accordion-group')) {
-    		$(this).parent().siblings('.ter-accordion').removeClass('is-expanded');
-    		$(this).parent().siblings('.ter-accordion').find('.ter-accordion__content').slideUp(200);
-    	}
-    	// Adjust the one you clicked
-    	$(this).parent().toggleClass('is-expanded');
-    	$(this).next('.ter-accordion__content').slideToggle(200);
+
+        // accordions
+        if ($(this).parent().hasClass('ter-accordion')) {
+            // Collapse the sibling accordions if this has a group
+            var maybeGroup = $(this).parent().parent();
+            if(maybeGroup && maybeGroup.hasClass('ter-accordion-group')) {
+                $(this).parent().siblings('.ter-accordion').removeClass('is-expanded');
+                $(this).parent().siblings('.ter-accordion').find('.ter-accordion__content').slideUp(200);
+            }
+            // Adjust the one you clicked
+            $(this).parent().toggleClass('is-expanded');
+            $(this).next('.ter-accordion__content').slideToggle(200);
+        }
+        // all other collapse instances
+        else {
+
+            var targetName = $(this).attr('data-target');
+            $(targetName).slideToggle().toggleClass('is-open');
+
+        }
+
     });
 
 })(jQuery, window, document);
