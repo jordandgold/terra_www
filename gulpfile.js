8953d2371970a150b729003c56f296600895a030
Jordan Gold <jordandgold@gmail.com>
2018-08-20 13:17:29 -0600

    update gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var nano = require('gulp-cssnano');
 var DEST = 'build/';
 var concat = require('gulp-concat');
 
-gulp.task('sass', function () {
+gulp.task('prod:sass', function () {
   var processors = [
       autoprefixer({browsers: ['last 1 version']}),
   ];
@@ -16,6 +16,10 @@ gulp.task('sass', function () {
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(processors))
     .pipe(gulp.dest('./public/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./public/scss/**/*.scss', ['prod:sass']);
 });
 
 gulp.task('sass:dist', function () {
@@ -25,26 +29,13 @@ gulp.task('sass:dist', function () {
   return gulp.src('./public/terra/scss/terra.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(processors))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('css', ['sass'], function () {
-    var processors = [
-        autoprefixer({browsers: ['last 1 version']}),
-    ];
-    return gulp.src('./public/css/*.css')
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('./public/css'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./public/scss/**/*.scss', ['sass']);
-});
-
-gulp.task('build', ['sass', 'css']);
-
-gulp.task('scripts', function() {
+gulp.task('scripts:dist', function() {
   return gulp.src('./public/terra/js/*.js')
     .pipe(concat('terra.js'))
     .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('terra:build', ['sass:dist', 'scripts:dist']);
