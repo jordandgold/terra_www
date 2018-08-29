41ce229d6dbc448ca52fc12f408c792fc314a02d
Jordan Gold <jordandgold@gmail.com>
2018-08-29 15:17:38 -0600

    refactor nav script

--- dist/terra.js
+++ dist/terra.js
@@ -137,98 +137,54 @@
   
 	'use strict';
 
-	var mainHeader = $('nav.navbar:not(.stay-put)'),
-		headerHeight = mainHeader.height(),
-		navToggle = $('.navbar__toggle');
-	
+	var navBar = $('nav.navbar.js-auto-hide'),
+		navBarHeight = navBar.height();
+
 	var scrolling = false,
 		previousTop = 0,
 		currentTop = 0,
 		scrollDelta = 10,
 		scrollOffset = 150;
 
-	mainHeader.on('click', '.nav-trigger', function(e){
+	navBar.on('click', '.navbar__toggle', function(e){
 		e.preventDefault();
-		mainHeader.toggleClass('nav-open');
+		navBar.toggleClass('is-open');
+		$(this).toggleClass('is-active');
+    	$(this).parent().children('.navbar__nav').slideToggle().toggleClass('is-expanded');
 	});
 
 	$(window).on('scroll', function(){
-		if( !scrolling ) {
+
+		if(!scrolling) {
+
 			scrolling = true;
+
 			(!window.requestAnimationFrame)
-				? setTimeout(autoHideHeader, 250)
-				: requestAnimationFrame(autoHideHeader);
+				? setTimeout(hideNavBar, 250)
+				: requestAnimationFrame(hideNavBar);
+
 		}
+
 	});
 
 	$(window).on('resize', function(){
-		headerHeight = mainHeader.height();
+		navBarHeight = navBar.height();
 	});
 
-	navToggle.click(function () {
-    	$(this).toggleClass('is-active');
-    	$(this).parent().children('.navbar__nav').slideToggle().toggleClass('is-expanded');
-    });
+	function hideNavBar() {
 
-	function autoHideHeader() {
 		var currentTop = $(window).scrollTop();
 
-		( belowNavHeroContent.length > 0 ) 
-			? checkStickyNavigation(currentTop) // secondary navigation below intro
-			: checkSimpleNavigation(currentTop);
+		if (previousTop - currentTop > scrollDelta) {
+	    	//if scrolling up
+	    	navBar.removeClass('is-hidden');
+	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+	    	//if scrolling down
+	    	navBar.addClass('is-hidden');
+	    }
 
 	   	previousTop = currentTop;
 		scrolling = false;
-	}
-
-	function checkSimpleNavigation(currentTop) {
-		//there's no secondary nav or secondary nav is below primary nav
-	    if (previousTop - currentTop > scrollDelta) {
-	    	//if scrolling up...
-	    	mainHeader.removeClass('is-hidden');
-	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
-	    	//if scrolling down...
-	    	mainHeader.addClass('is-hidden');
-	    	//close dropdowns
-	    	mainHeader.find('.navbar__nav').find('.dropdown.open').each(function(){
-	    		$(this).removeClass('open');
-	    	});
-	    }
-	}
-
-	function checkStickyNavigation(currentTop) {
-		//secondary nav below intro section - sticky secondary nav
-		var secondaryNavOffsetTop = belowNavHeroContent.offset().top - secondaryNavigation.height() - mainHeader.height();
-		
-		if (previousTop >= currentTop ) {
-	    	//if scrolling up... 
-	    	if( currentTop < secondaryNavOffsetTop ) {
-	    		//secondary nav is not fixed
-	    		mainHeader.removeClass('is-hidden');
-	    		secondaryNavigation.removeClass('fixed slide-up');
-	    		belowNavHeroContent.removeClass('secondary-nav-fixed');
-	    	} else if( previousTop - currentTop > scrollDelta ) {
-	    		//secondary nav is fixed
-	    		mainHeader.removeClass('is-hidden');
-	    		secondaryNavigation.removeClass('slide-up').addClass('fixed'); 
-	    		belowNavHeroContent.addClass('secondary-nav-fixed');
-	    	}
-	    	
-	    } else {
-	    	//if scrolling down...	
-	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset ) {
-	 	  		//hide primary nav
-	    		mainHeader.addClass('is-hidden');
-	    		secondaryNavigation.addClass('fixed slide-up');
-	    		belowNavHeroContent.addClass('secondary-nav-fixed');
-	    	} else if( currentTop > secondaryNavOffsetTop ) {
-	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset 
-	    		mainHeader.removeClass('is-hidden');
-	    		secondaryNavigation.addClass('fixed').removeClass('slide-up');
-	    		belowNavHeroContent.addClass('secondary-nav-fixed');
-	    	}
-
-	    }
 	}
 
 })(jQuery, window, document);
