4c7e40a8d34cb9384bb9ced0a914a3740fb9c4a2
Jordan Gold <jordandgold@gmail.com>
2018-08-29 15:35:59 -0600

    BEM refactor all navs

--- dist/terra.js
+++ dist/terra.js
@@ -458,9 +458,9 @@
   
 	'use strict';
 
-	$('.tree-menu__list li.is-expanded').find('ul').show();
+	$('.ter-tree-menu__list li.is-expanded').find('ul').show();
 
-    $('.tree-menu__list li.is-expandable > a').click(function () {
+    $('.ter-tree-menu__list li.is-expandable > a').click(function () {
     	if ($(this).parent().hasClass('is-expanded')) {
     		$(this).next('ul').slideToggle(200).parent().toggleClass('is-expanded');
     	} else {
