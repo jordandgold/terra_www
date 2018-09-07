c9f0f5d9b254d4487bdaabb575b2f4648d9af84a
Jordan Gold <jordandgold@gmail.com>
2018-09-07 14:46:49 -0600

    push tabs module

--- public/terra/js/tabs.js
+++ public/terra/js/tabs.js
@@ -7,4 +7,32 @@
 
 	'use strict';
 
+	var tabItems = $('.ter-tabs__nav a');
+
+	tabItems.on('click', function(event){
+
+		event.preventDefault();
+
+		var selectedItem = $(this);
+
+		if( !selectedItem.hasClass('is-active') ) {
+
+			var selectedTab = selectedItem.data('content'),
+				tabScope = selectedItem.parent().parent().parent().data('controls'),
+				tabContentWrapper = $(tabScope),
+				selectedContent = tabContentWrapper.find('li[data-content="'+selectedTab+'"]'),
+				slectedContentHeight = selectedContent.innerHeight();
+
+				console.log(tabScope);
+			
+			$('[data-controls="'+tabScope+'"]').find('a[data-content]').parent().removeClass('is-active');
+			selectedItem.parent().addClass('is-active');
+			selectedContent.addClass('is-active').siblings('li').removeClass('is-active');
+
+			tabContentWrapper.animate({
+				'height': slectedContentHeight
+			}, 200);
+		}
+	});
+
 })(jQuery, window, document);
