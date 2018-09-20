4292e4b624234677bbfb457a0bd881fa4c2d1467
Jordan Gold <jordandgold@gmail.com>
2018-09-20 16:01:55 -0600

    tooltip mobile optimization

--- public/terra/js/tooltip.js
+++ public/terra/js/tooltip.js
@@ -7,74 +7,36 @@
 	
 	'use strict';
 
-	var targets = $( '[rel~=tooltip]' ),
-        target  = false,
-        tooltip = false,
-        title   = false;
- 
-    targets.bind('mouseenter', function() {
+	var tooltips = $('.ter-tooltip'),
+		target,
+		tooltip,
+		tooltipArrow;
 
-        target  = $(this);
-        tip     = target.attr('title');
-        tooltip = $('<div id="tooltip"></div>');
- 
-        if( !tip || tip == '' )return false;
- 
-        target.removeAttr( 'title' );
-        tooltip.css( 'display', 'none' )
-               .html( tip )
-               .appendTo( 'body' );
- 
-        var init_tooltip = function()
-        {
-            if( $( window ).width() < tooltip.outerWidth() * 1.5 )
-                tooltip.css( 'max-width', $( window ).width() / 2 );
-            else
-                tooltip.css( 'max-width', 340 );
- 
-            var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
-                pos_top  = target.offset().top - tooltip.outerHeight() - 20 - $(window).scrollTop();
- 
-            if( pos_left < 0 )
-            {
-                pos_left = target.offset().left + target.outerWidth() / 2 - 20;
-                tooltip.addClass( 'left' );
-            }
-            else
-                tooltip.removeClass( 'left' );
- 
-            if( pos_left + tooltip.outerWidth() > $( window ).width() )
-            {
-                pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
-                tooltip.addClass( 'right' );
-            }
-            else
-                tooltip.removeClass( 'right' );
- 
-            if( pos_top < 0 )
-            {
-                var pos_top  = target.offset().top + target.outerHeight();
-                tooltip.addClass( 'top' );
-            }
-            else
-                tooltip.removeClass( 'top' );
- 
- 
- 			//console.log(pos_left,pos_top );
-            tooltip.css( { left: pos_left, top: pos_top } )
-                   .show();
-        };
- 
-        init_tooltip();
-        $( window ).resize( init_tooltip );
- 
-        var remove_tooltip = function() {
-            tooltip.remove();
-            target.attr( 'title', tip );
-        };
- 
-        target.bind( 'mouseleave', remove_tooltip );
-        tooltip.bind( 'click', remove_tooltip );
-    });
+	tooltips.bind('mouseenter', function() {
+
+		target  = $(this);
+        tooltip = target.children('.ter-tooltip__content');
+
+		var initTooltip = function() {
+
+			var pos_top  = tooltip.offset().top,
+				pos_left = tooltip.offset().left,
+	            pos_bottom = tooltip.offset().bottom,
+	            pos_right = ($(window).width() - (tooltip.offset().left + tooltip.outerWidth()));
+	 
+	        if (pos_left < 0) {
+	            tooltip.addClass('bump-left');
+	        }
+
+	        if (pos_right < 0) {
+	            tooltip.addClass('bump-right');
+	        }
+
+		}
+
+		initTooltip();
+    	$(window).resize(initTooltip);
+
+	});
 	
-});
+})(jQuery, window, document);
