f5c62f28a672387889609e5bc702262627e58da6
Shawn Williams <shawn_williams@trimble.com>
2019-02-01 10:20:01 -0700

    Adding https redirect to server.default.js

--- server.default.js
+++ server.default.js
@@ -39,6 +39,14 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(function(req, res, next) {
+    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+    }
+    else
+        next();
+});
+
 
 
 function createPages() {
